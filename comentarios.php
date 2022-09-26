9818d3f4ba0fb85dbb83220cc7ce2c25fd4a6905
Wesley dos Anjos <wesleyanjosprogramming@outlook.com.br>
2022-09-26 17:21:42 -0300

    comentários

---
+++ comentarios.php
@@ -0,0 +1,19 @@
+<?php
+// Isto é um comentário de linha ignore
+# Isto é um comentário de linha ignore
+
+/*
+Isto é um comentário também.
+Este tipo de comentário pode ser usado para quando é
+necessário descrever com mais detalhes uma função para
+você ou outro desenvolvedor(a)/programador(a).
+Fim da linha
+*/
+
+/*
+$x = 10;
+$y = 15;
+function Somar($a, $b){return $a + $b;}
+echo Somar($x,$y);
+*/
+?>
